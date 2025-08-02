6f4b822a30e338267228f4e47ab0155c84628037
Kendrick <mcadams.kendrick@gmail.com>
2025-08-02 18:09:50 -0500

    resolving issue with unfree packages

--- flake.nix
+++ flake.nix
@@ -1,24 +1,29 @@
+# flake.nix
 {
-	description = "My Basic NixOS Config";
-	
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		home-manager.url = "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-	};
+  description = "My Basic NixOS Config";
 
-	outputs = { self, nixpkgs, home-manager, ... }: {
-		nixosConfigurations.my-basic-nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				{ nixpkgs.config.allowUnfree = true;  }
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager.backupFileExtension = "bak";
-					home-manager.users.km = import ./home.nix;
-				}
-			];
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.my-basic-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+		{ nixpkgs.config.allowUnfree = true; }
+        
+		./configuration.nix
+    
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.backupFileExtension = "bak";
+          home-manager.users.km = import ./home.nix;
+        }
+      ];
+    };
+  };
 }
