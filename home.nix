2fdf5f07061cc24808c227729aa34ba9fe500250
Kendrick <mcadams.kendrick@gmail.com>
2025-08-03 14:17:31 -0500

    Moving discord declaration to the home.nix

--- home.nix
+++ home.nix
@@ -5,13 +5,16 @@
   home.homeDirectory = "/home/km";
   home.stateVersion = "25.11"; # Make sure this matches your NixOS version
 
+  home.packages = with pkgs; [
+    discord
+  ];
+
   # Import individual Home Manager modules for specific applications/services
   imports = [
     # Applications
     ./modules/home-manager/applications/git.nix
     ./modules/home-manager/applications/vscode.nix
     ./modules/home-manager/applications/firefox.nix
-    ./modules/home-manager/applications/discord.nix
 
     # Shell Configuration
     ./modules/home-manager/low_level/bash.nix
