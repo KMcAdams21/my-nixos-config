373775280d075b48cee5b928852094bd3b2c27b0
Kendrick <mcadams.kendrick@gmail.com>
2026-01-09 16:21:23 -0600

    removing claude code and adding antrigrav

--- modules/home-manager/applications/vscode.nix
+++ modules/home-manager/applications/vscode.nix
@@ -6,13 +6,11 @@
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
         enkia.tokyo-night
-        anthropic.claude-code
         Google.gemini-cli-vscode-ide-companion
       ];
       
       userSettings = {
         "workbench.colorTheme" = "Tokyo Night";        
-        "claude.autoSave" = true;
         "gemini.enable" = true;
       };
     };
