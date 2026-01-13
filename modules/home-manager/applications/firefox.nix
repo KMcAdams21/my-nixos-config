d005f5b74b54d14caab5dee473797a3fd1074882
Kendrick <mcadams.kendrick@gmail.com>
2026-01-12 20:43:57 -0600

    editing tab wrangler name

--- modules/home-manager/applications/firefox.nix
+++ modules/home-manager/applications/firefox.nix
@@ -18,7 +18,7 @@
         packages = with pkgs.nur.repos.rycee.firefox-addons; [
           violentmonkey
           onepassword-password-manager
-          tab-wrangler
+          tabwrangler
         ];
       };
     };
