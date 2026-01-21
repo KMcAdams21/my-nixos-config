728a9ea7c6026356b54ac505fc4e7bb614045605
Kendrick <mcadams.kendrick@gmail.com>
2026-01-20 21:36:40 -0600

    temp getting rid of gamescope

--- modules/config/steam.nix
+++ modules/config/steam.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Gamescope compositor with elevated privileges for better performance
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;  # Allows better scheduling priority for games
-  };
 
   programs.steam = {
     enable = true;
@@ -15,15 +10,7 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
 
-    # Enable Gamescope session (Steam Deck-like experience)
-    gamescopeSession.enable = true;
-
     # Include proton compatibility packages
     extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
-
-  # Required for HDR support in Gamescope
-  environment.systemPackages = with pkgs; [
-    gamescope-wsi
-  ];
 }
