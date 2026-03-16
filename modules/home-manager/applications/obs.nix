9b093b8396bcdbb2286d23cc99faa61f35b15bee
Kendrick <mcadams.kendrick@gmail.com>
2026-03-16 18:53:36 -0500

    adding obs

--- modules/home-manager/applications/obs.nix
+++ modules/home-manager/applications/obs.nix
@@ -1,8 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    obs-studio
-    (obs-studio-plugins.obs-backgroundremoval)
-  ];
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+    ];
+  };
 }
